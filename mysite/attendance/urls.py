a7947b0147289b256dbf9f7a1cef2c4ec95fa195
kamisul582 <kamil.sulzycki@op.pl>
2023-10-14 11:07:40 +0200

    views + gitignore

--- mysite/attendance/urls.py
+++ mysite/attendance/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:user_id>/user_info",views.user_info, name="user_info")
+    path("<int:user_id>/user_info",views.user_info, name="user_info"),
+    path("<int:company_id>/company_info",views.company_info, name="company_info")
 ]
--- mysite/attendance/views.py
+++ mysite/attendance/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User
+from .models import User, Company
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the attendance index.")
 
 def user_info(request, user_id):
-    user = User.objects.get(pk=id)
-    return HttpResponse(f"The User info is:{User}")# {temp_user.email}, {temp_user.name}, {temp_user.surname}")
-             
+    user = User.objects.get(pk=user_id)
+    return HttpResponse(user)
+
+
+
+def company_info(request, company_id):
+    company = Company.objects.get(pk=company_id)
+    return HttpResponse(company)
 #def user_info(request, user_id):
  #   User
  #   return HttpResponse(f"The User info is:{user_id}")
