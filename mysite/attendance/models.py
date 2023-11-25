3813e06f925b0741dfc8dbe8e3ae71f921007b8a
kamisul582 <kamil.sulzycki@op.pl>
2023-11-25 10:24:46 +0100

    Basic login fixed, db re-done

--- mysite/attendance/models.py
+++ mysite/attendance/models.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.models import UserManager, AbstractBaseUser
 
 class Company(models.Model):
     def __str__(self) -> str:
@@ -9,18 +10,21 @@ class Company(models.Model):
     company_name = models.CharField(max_length=200)
     company_address = models.CharField(max_length=200)
 
-class User(models.Model):
+class User(AbstractBaseUser):
     REQUIRED_FIELDS = ('user',)
     USERNAME_FIELD = 'email'
+    is_authenticated = True
     is_anonymous = False
     def __str__(self) -> str:
         return self.email + " " + self.name + " " + self.surname + " " + self.kiosk_code
-    email = models.CharField(max_length=200, default=False,)
+    email = models.CharField(max_length=200, default=False, unique=True)
     password = models.CharField(max_length=200, default=False)
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     employer  = models.ForeignKey(Company, on_delete=models.CASCADE)
     kiosk_code = models.CharField(max_length=10)
+    last_login = models.DateField(auto_now_add=True)
+    objects = UserManager()
 
 class WorkTime(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
