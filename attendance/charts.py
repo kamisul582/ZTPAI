ab1a2306d363d26d99eb3f0061a22e10dabaa736
kamisul582 <kamil.sulzycki@op.pl>
2024-03-12 15:07:20 +0100

    Charts in progress, added link, works with worktime on hardcoded worker_id

--- attendance/charts.py
+++ attendance/charts.py
@@ -1,23 +1,40 @@
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
-
+from .models import CustomUser, Worktime, Company, Worker
 
 class LineChartJSONView(BaseLineChartView):
+    extra_data = None  # Define a class variable to store extra data
+    user_id = None
     def get_labels(self):
-        """Return 7 labels for the x-axis."""
-        return ["January", "February", "March", "April", "May", "June", "July"]
+        """Return labels for the x-axis based on your model data and extra data."""
+        # Replace this with your actual logic to get date labels from your model
+        worker = Worker.objects.get(user_id=self.user_id)
+        labels_from_model = [str(entry.date) for entry in Worktime.objects.filter(worker_id=worker)]
+
+        if self.extra_data:
+            # Combine model data labels with extra data labels
+            labels_from_model += self.extra_data.get('labels', [])
+
+        return labels_from_model
 
     def get_providers(self):
         """Return names of datasets."""
         return ["Central", "Eastside", "Westside"]
 
     def get_data(self):
-        """Return 3 datasets to plot."""
+        """Return datasets to plot based on your model data and extra data."""
+        worker = Worker.objects.get(user_id=self.user_id)
+        total_time = [entry.total_time.total_seconds()/60 for entry in Worktime.objects.filter(worker_id=worker)]
+        punch_in = [entry.punch_in for entry in Worktime.objects.filter(worker_id=worker)]
+        punch_out = [entry.punch_out for entry in Worktime.objects.filter(worker_id=worker)]
+        name_data = [entry.name for entry in Company.objects.all()]
 
-        return [[75, 44, 92, 11, 44, 95, 35],
-                [41, 92, 18, 3, 73, 87, 92],
-                [87, 21, 94, 3, 90, 13, 65]]
+        if self.extra_data:
+            # Combine model data with extra data
+            total_time += self.extra_data.get('central', [])
+            name_data += self.extra_data.get('eastside', [])
+        
+        return [total_time]
 
-
-line_chart = TemplateView.as_view(template_name='attendance/charts.html')
-line_chart_json = LineChartJSONView.as_view()
+template = TemplateView.as_view(template_name='attendance/charts.html')
+line_chart_json = LineChartJSONView.as_view(user_id = 2)
--- attendance/urls.py
+++ attendance/urls.py
@@ -16,7 +16,7 @@ from .views import (
     get_employees,
     get_worker_worktime,
 )
-from .charts import line_chart, line_chart_json
+from .charts import template, line_chart_json
 # router = DefaultRouter()
 # router.register('data', RegistrationWizardView, basename='data')
 # urlpatterns = router.urls
@@ -42,7 +42,7 @@ urlpatterns = [
     path('get-employees/', get_employees, name='get_employees'),
     path('get-employees/<str:sort>/<str:filter>/', get_employees, name='get_employees'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('charts', line_chart, name='line_chart'),
+    path('charts', template, name='charts'),
     path('chartJSON', line_chart_json, name='line_chart_json'),
 ]
 
